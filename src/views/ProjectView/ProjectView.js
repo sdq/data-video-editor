0569d1215ef19c18899a7b990e725c1f587f26a7
sdq <sdq@tongji.edu.cn>
2019-08-05 09:58:23 +0800

    add projectview

--- src/views/ProjectView/ProjectView.js
+++ src/views/ProjectView/ProjectView.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import HeaderBar from '../HeaderBar';
+import './projectview.css';
 
 export default class ProjectView extends Component {
     render() {
         return (
             <div>
+                <HeaderBar/>
                 ProjectView
             </div>
         )
