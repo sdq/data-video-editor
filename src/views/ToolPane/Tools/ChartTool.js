a0e4fc4e68efbe15f2a0b99fd1e5bc5abb658322
sdq <sdq@tongji.edu.cn>
2019-08-08 19:23:46 +0800

    add tools

---
+++ src/views/ToolPane/Tools/ChartTool.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ChartTool extends Component {
+    render() {
+        return (
+            <div>
+                ChartTool
+            </div>
+        )
+    }
+}
---
+++ src/views/ToolPane/Tools/TextTool.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TextTool extends Component {
+    render() {
+        return (
+            <div>
+                TextTool
+            </div>
+        )
+    }
+}
