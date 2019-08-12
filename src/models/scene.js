58351c073ac839a8531958bbec379e6641635b9d
sdq <sdq@tongji.edu.cn>
2019-08-12 10:14:23 +0800

    remove scene.js

--- src/models/scene.js
+++
@@ -1,8 +0,0 @@
-export default class Scene {
-    constructor(script, dataurl, elements, duration) {
-        this.script = script;
-        this.dataurl = dataurl;
-        this.elements = elements;
-        this.duration = duration;
-    }
-}
