e883451f00dcb854c4a941b308e1340e10f98b03
sdq <sdq@tongji.edu.cn>
2019-08-16 07:45:56 +0800

    Add Data Actions

--- src/actions/visAction.js
+++ src/actions/visAction.js
@@ -1,5 +1,17 @@
 import VisActionType from '../constants/VisActionType';
 
+// Data
+export const uploadData = (csvfile) => ({
+    type: VisActionType.UPLOAD_DATA,
+    csvfile
+})
+
+export const changeData = (index) => ({
+    type: VisActionType.CHANGE_DATA,
+    index
+})
+
+// Exploration
 export const encoding = (channel, field) => ({
     type: VisActionType.ENCODING,
     channel,
@@ -16,4 +28,26 @@ export const removeEncoding = (channel, field) => ({
     type: VisActionType.REMOVE_ENCODING,
     channel,
     field
+})
+
+// Meta
+export const undo = () => ({
+    type: VisActionType.UNDO_VIS
+})
+
+export const redo = () => ({
+    type: VisActionType.REDO_VIS
+})
+
+// Insight
+export const bookmark = (spec, note, time) => ({
+    type: VisActionType.BOOKMARK,
+    spec,
+    note,
+    time
+})
+
+export const save = (spec) => ({
+    type: VisActionType.SAVE,
+    spec
 })
--- src/selectors/vis.js
+++ src/selectors/vis.js
@@ -1,9 +1,31 @@
 import { createSelector } from 'reselect';
 import ElementType from '@/constants/ElementType';
 
+// Data
+const dataList = state => state.dataReducer.dataList;
+const fieldsList = state => state.dataReducer.fieldsList;
+const dataIndex = state => state.dataReducer.dataIndex;
+
+// Chart
 const elementIndex = state => state.canvas.elementIndex;
 const scenes = state => state.timeline.scenes;
 const sceneIndex = state => state.timeline.index;
+
+export const getCurrentData = createSelector(
+    dataList,
+    dataIndex,
+    (dataList, dataIndex) => {
+        return {
+            values: dataList[dataIndex]
+        };
+    }
+)
+
+export const getCurrentFields = createSelector(
+    fieldsList,
+    dataIndex,
+    (fieldsList, dataIndex) => fieldsList[dataIndex]
+)
 
 export const currenVis = createSelector(
     scenes,
