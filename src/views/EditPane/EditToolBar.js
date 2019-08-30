03137bc4127e7e1c1009722a78e405504e48d1a8
sdq <sdq@tongji.edu.cn>
2019-08-30 20:54:37 +0800

    edit tool bar

--- src/views/EditPane/EditToolBar.js
+++ src/views/EditPane/EditToolBar.js
@@ -30,28 +30,28 @@ export default class EditToolBar extends Component {
     }
 
     render() {
-        const { isElementSelected, copiedElement } = this.props;
+        const { isElementSelected, copiedElement, isPerforming } = this.props;
         return (
             <div> 
                 <ButtonGroup style = { {margin: '10px 0px 0px 20px', float:'left'} }>
-                    <Button icon="copy" style = { {padding: '0 20px 0 20px'} } onClick={this.copyElement} disabled={!isElementSelected}/>
-                    <Button icon="scissor" style = { {padding: '0 20px 0 20px'} } onClick={this.cutElement} disabled={!isElementSelected}/>
-                    <Button icon="snippets" style = { {padding: '0 20px 0 20px'} } onClick={this.pasteElement} disabled={copiedElement === null}/>
-                    <Button icon="delete" style = { {padding: '0 20px 0 20px'} } onClick={this.deleteElement} disabled={!isElementSelected}/>
+                    <Button icon="copy" style = { {padding: '0 20px 0 20px'} } onClick={this.copyElement} disabled={!isElementSelected || isPerforming}/>
+                    <Button icon="scissor" style = { {padding: '0 20px 0 20px'} } onClick={this.cutElement} disabled={!isElementSelected || isPerforming}/>
+                    <Button icon="snippets" style = { {padding: '0 20px 0 20px'} } onClick={this.pasteElement} disabled={copiedElement === null || isPerforming}/>
+                    <Button icon="delete" style = { {padding: '0 20px 0 20px'} } onClick={this.deleteElement} disabled={!isElementSelected || isPerforming}/>
                  </ButtonGroup>
                 
-                <ButtonGroup style = { {margin: '10px 20px 0 0', float:'right'} }>
-                    <Button icon="undo" style = { {padding: '0 20px 0 20px'} }/>
-                    <Button icon="redo" style = { {padding: '0 20px 0 20px'} }/>
+                <ButtonGroup style = { {margin: '10px 20px 0 0', float:'right'} } >
+                    <Button icon="undo" style = { {padding: '0 20px 0 20px'} } disabled={isPerforming}/>
+                    <Button icon="redo" style = { {padding: '0 20px 0 20px'} } disabled={isPerforming}/>
                 </ButtonGroup>
 
                 <ButtonGroup style = { {margin: '10px 20px 0px 0px', float:'right'} }>
                    
-                    <Button icon="pic-center" style = { {padding: '0 20px 0 20px'} }/>
-                    <Button icon="table" style = { {padding: '0 20px 0 20px'} }/>
+                    <Button icon="pic-center" style = { {padding: '0 20px 0 20px'} } disabled={isPerforming}/>
+                    <Button icon="table" style = { {padding: '0 20px 0 20px'} } disabled={isPerforming}/>
                  </ButtonGroup>
 
-                 <Button icon="font-size" style = { {width:'80px',margin: '10px 20px 0px 0px', float:'right'} }/>
+                 <Button icon="font-size" style = { {width:'80px',margin: '10px 20px 0px 0px', float:'right'} } disabled={isPerforming}/>
             </div>
         )
     }
