a472d93e1e0e10be505fa29a984432b63b863877
sdq <sdq@tongji.edu.cn>
2019-09-03 17:23:41 +0800

    canvas preview

---
+++ src/components/SceneBlock/CanvasPreview.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Color from '@/constants/Color';
+import { Stage, Layer, Rect } from 'react-konva';
+import ImageElement from '@/components/Elements/ImageElement';
+import TextElement from '@/components/Elements/TextElement';
+import ChartElement from '@/components/Elements/ChartElement';
+import ElementType from '@/constants/ElementType';
+
+export default class CanvasPreview extends Component {
+    render() {
+        return (
+            <div>
+                <Stage width={192} height={108} scale={{x: 192/800, y:192/800}}>
+                    <Layer>
+                        <Rect
+                            x={0}
+                            y={0}
+                            width={800}
+                            height={450}
+                            fill={this.props.scene.backgroundColor()}
+                        />
+                    </Layer>
+                    <Layer>
+                        {this.props.scene.elements().map(function(element, index) {
+                            //console.log(element.info());
+                            switch (element.type()) {
+                                case ElementType.TEXT:
+                                    return <TextElement key={this.props.sceneIndex+"-"+index} element={element} name={this.props.sceneIndex+"-"+index} draggable = {false} {...this.props}/>
+                                case ElementType.IMAGE:
+                                    return <ImageElement key={this.props.sceneIndex+"-"+index} element={element} name={this.props.sceneIndex+"-"+index} draggable = {false} {...this.props}/>
+                                case ElementType.CHART:
+                                    return <ChartElement key={this.props.sceneIndex+"-"+index} element={element} name={this.props.sceneIndex+"-"+index}  width={200} height={200} draggable = {false} {...this.props}/>
+                                case ElementType.AUDIO:
+                                    return null;
+                                default:
+                                    //TODO: remove
+                                    console.log("wrong!!!!!!!");
+                                    console.log(this.props.currentScene.elements());
+                                    console.log(element);
+                                    return;
+                            }
+                        }.bind(this))}
+                    </Layer>
+                </Stage>
+            </div>
+        )
+    }
+}
--- src/components/SceneBlock/index.js
+++ src/components/SceneBlock/index.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import Color from '@/constants/Color';
-import { Stage, Layer } from 'react-konva';
-import ImageElement from '@/components/Elements/ImageElement';
-import TextElement from '@/components/Elements/TextElement';
-import ChartElement from '@/components/Elements/ChartElement';
-import ElementType from '@/constants/ElementType';
+import CanvasPreview from './CanvasPreview';
 import UIMode from '@/constants/UIMode';
 import './sceneblock.css';
 
@@ -33,32 +29,10 @@ export default class SceneBlock extends Component {
     render() {
         return (
             <div className="sceneblock" onClick={this.clickSceneBlock} onDoubleClick={this.dbclickSceneBlock} style={{borderColor: this.props.isSelected?Color.DEEP_ORANGE:Color.GRAY}}>
-                <div className="canvasPreview">
-                    <Stage width={192} height={108} scale={{x: 192/800, y:192/800}}>
-                        <Layer>
-                            {this.props.scene.elements().map(function(element, index) {
-                                //console.log(element.info());
-                                switch (element.type()) {
-                                    case ElementType.TEXT:
-                                        return <TextElement key={this.props.sceneIndex+"-"+index} element={element} name={this.props.sceneIndex+"-"+index} draggable = {false} {...this.props}/>
-                                    case ElementType.IMAGE:
-                                        return <ImageElement key={this.props.sceneIndex+"-"+index} element={element} name={this.props.sceneIndex+"-"+index} draggable = {false} {...this.props}/>
-                                    case ElementType.CHART:
-                                        return <ChartElement key={this.props.sceneIndex+"-"+index} element={element} name={this.props.sceneIndex+"-"+index}  width={200} height={200} draggable = {false} {...this.props}/>
-                                    case ElementType.AUDIO:
-                                        return null;
-                                    default:
-                                        //TODO: remove
-                                        console.log("wrong!!!!!!!");
-                                        console.log(this.props.currentScene.elements());
-                                        console.log(element);
-                                        return;
-                                }
-                            }.bind(this))}
-                        </Layer>
-                    </Stage>
+                <div className="canvas-preview">
+                    <CanvasPreview {...this.props}/>
                 </div>
-                <div className="scriptPreview">
+                <div className="script-preview">
                     {this.props.scene.script()}
                 </div>
             </div>
