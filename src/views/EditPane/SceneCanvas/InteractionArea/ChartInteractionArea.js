3d52e29ee266faa4dae8161f694d85c23adce16d
sdq <sdq@tongji.edu.cn>
2019-10-13 21:46:59 +0800

    add barchart demo

---
+++ src/views/EditPane/SceneCanvas/InteractionArea/ChartInteractionArea.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import BarChart from '@/charts/BarChart';
+
+export default class ChartInteractionArea extends Component {
+    render() {
+        return (
+            <div>
+                <BarChart />
+            </div>
+        )
+    }
+}
