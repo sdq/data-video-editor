397cee241527304cdee0b6be7ac431788bbf1983
sdq <sdq@tongji.edu.cn>
2019-10-21 21:36:16 -0700

    update demo story

--- src/reducers/demo.js
+++ src/reducers/demo.js
@@ -1,19 +1,55 @@
 import Scene from '../models/Scene';
 import Video from '../models/Video';
-import { Element, ImageInfo, ChartInfo, TextInfo} from '../models/Element';
+import { Element, ImageInfo, ChartInfo, TextInfo , AudioInfo} from '../models/Element';
 import MyURL from '../constants/MyURL';
 import ElementType from '../constants/ElementType';
 import ChartType from '../constants/ChartType';
 import ChartCategory from '../constants/ChartCategory';
-// import AnimationType from '../animation/AnimationType';
-// import AnimationModel from '../animation/AnimationModel';
+import AnimationType from '../animation/AnimationType';
+import AnimationModel from '../animation/AnimationModel';
 import Color from '../constants/Color';
-
+//var gifFrames = require('gif-frames');
 // Demo
+
+const demoaudio1 = new AudioInfo(
+    '88mp3.mp3',
+    MyURL.OSS+'/images/88mp3.mp3',
+    4,
+)
+
+const demoimage01 = new ImageInfo(
+    'eu.png',
+    MyURL.OSS+'/images/eu.png',
+    200,
+    90,
+    80,
+    50,
+    0,
+)
+const demoimage002 = new ImageInfo(
+    'usa.png',
+    MyURL.OSS+'/images/usa.png',
+    200,
+    150,
+    80,
+    50,
+    0,
+)
+const demoimage03 = new ImageInfo(
+    'japan.png',
+    MyURL.OSS+'/images/japan.png',
+    200,
+    220,
+    80,
+    50,
+    0,
+)
+
+
 const demoimage = new ImageInfo(
     'work.png',
     MyURL.OSS+'/images/vehicle.png',
-    250,
+    320,
     90,
     300,
     167,
@@ -21,23 +57,44 @@ const demoimage = new ImageInfo(
 )
 
 const demotext = new TextInfo(
-    "英美日小轿车比较",
-    250,
+    "英美日三国小轿车比较",
+    220,
     350,
     0,
     'black',
     40,
 )
 
+const element00 = new Element(ElementType.AUDIO, demoaudio1);
+const element01 = new Element(ElementType.IMAGE, demoimage01);
+const element002 = new Element(ElementType.IMAGE, demoimage002);
+const element03 = new Element(ElementType.IMAGE, demoimage03);
 const element1 = new Element(ElementType.IMAGE, demoimage);
 const element2 = new Element(ElementType.TEXT, demotext);
-element1.duration(10.0);
-element2.duration(10.0);
-const scene1 = new Scene("Comparison of cars in three countries", 10.0);
+element01.duration(4.0);
+element002.duration(4.0);
+element03.duration(4.0);
+element1.duration(4.0);
+element2.duration(4.0);
+const animation0 = new AnimationModel(AnimationType.PRESENTATION_FADEIN, 'Zoomin');//改成flicker
+animation0.start(0);
+animation0.duration(3);
+element2.add(animation0);
+const scene1 = new Scene("Comparison of cars in three countries", 4.0);
+scene1.addElement(element00);
+scene1.addElement(element01);
+scene1.addElement(element002);
+scene1.addElement(element03);
 scene1.addElement(element1);
 scene1.addElement(element2);
 // scene1.backgroundColor(Color.CLEAR_BLUE);
 
+
+const demoaudio222 = new AudioInfo(
+    '90mp3.mp3',
+    MyURL.OSS+'/images/90mp3.mp3', 
+    4,
+)
 const demoimage2 = new ImageInfo(
     'sport.png',
     MyURL.OSS+'/images/blue-car.png',
@@ -82,30 +139,96 @@ const demotext22 = new TextInfo(
     'black',
     20,
 )
+//
+// let demogif21Frams;
+// async function parseGif(gifUrl) {
+//     let tempData;
+//     await gifFrames(
+//         { url: gifUrl, frames: 'all', outputType: 'canvas', cumulative: true },
+//         function (err, frameData) {
+//             if (err) {
+//                 throw err;
+//             }
+//             tempData= frameData;
+//         }
+//     );
+//     console.log("tempData",tempData)
+//     return tempData;
+// }
 
+
+
+// const demogif22 = new GifInfo(
+//     'walking.gif',
+//     MyURL.OSS+'/gifs/walking.gif',
+//     1,
+//     1,
+//     200,
+//     50,
+//     100,
+//     100,
+// )
+const element2222 = new Element(ElementType.AUDIO, demoaudio222);
 const element02 = new Element(ElementType.CHART, demochart2);
 const element12 = new Element(ElementType.IMAGE, demoimage2);
 const element21 = new Element(ElementType.TEXT, demotext21);
 const element22 = new Element(ElementType.TEXT, demotext22);
+
+element02.start(1.0);  //没用
+element12.start(1.0);
+element2222.duration(10.0);
 element02.duration(10.0);
 element12.duration(10.0);
 element21.duration(10.0);
 element22.duration(10.0);
-// const animation1 = new AnimationModel(AnimationType.PRESENTATION_FADE, 'Fade');
-// animation1.start(0);
-// animation1.duration(10);
-// element12.add(animation1);
-// const animation2 = new AnimationModel(AnimationType.PRESENTATION_ZOOM, 'Zoom');
-// animation2.start(10);
-// animation2.duration(10);
-// element12.add(animation2);
+
+//添加动画后图表爆炸  无法全部加上动画
+const animation1 = new AnimationModel(AnimationType.PRESENTATION_ZOOMIN, 'zoomin');
+animation1.start(0);
+animation1.duration(2);
+//element12.add(animation1);
+element02.add(animation1);
+//element21.add(animation1);
+//element22.add(animation1);
+
+
 
 
 const scene2 = new Scene("Comparison of car horsepower in three countries", 10.0);
+scene2.addElement(element2222);
 scene2.addElement(element02);
 scene2.addElement(element12);
 scene2.addElement(element22);
 scene2.addElement(element21);
+
+
+// let demogif21;
+// (async () => {
+//     demogif21Frams = await  parseGif(MyURL.OSS+'/gifs/walking.gif');
+//     console.log("demogif21Frams",demogif21Frams)
+//     demogif21 = new GifInfo(
+//         'walking.gif',
+//         MyURL.OSS+'/gifs/walking.gif',
+//         1,
+//         demogif21Frams,
+//         200,
+//         50,
+//         100,
+//         100,
+//     )
+
+//     const element221 = new Element(ElementType.GIF, demogif21);
+//     element221.duration(10.0);
+//     scene2.addElement(element221);
+// })();
+
+
+// const element222 = new Element(ElementType.GIF, demogif22);
+
+// element222.duration(10.0);
+
+// scene2.addElement(element222);
+
 // scene2.backgroundImage(MyURL.OSS+"/backgroundImages/s-classroom.png");
 
 const video = new Video(1);
@@ -113,6 +236,12 @@ video.id(2);
 video.add(scene1);
 video.add(scene2);
 
+
+const demoaudio333 = new AudioInfo(
+    '91mp3.mp3',
+    MyURL.OSS+'/images/91mp3.mp3', 
+    4,
+)
 
 const demoimage3 = new ImageInfo(
     'woman.png',
@@ -152,6 +281,8 @@ const demotext3 = new TextInfo(
     30,
 )
 
+
+
 const demotext31 = new TextInfo(
     "美国加速度范围略大",
     200,
@@ -161,15 +292,23 @@ const demotext31 = new TextInfo(
     20,
 )
 
+const element333 = new Element(ElementType.AUDIO, demoaudio333);
 const element31 = new Element(ElementType.CHART, demochart3);
 const element32 = new Element(ElementType.IMAGE, demoimage3);
 const element33 = new Element(ElementType.TEXT, demotext3);
 const element34 = new Element(ElementType.TEXT, demotext31);
+element333.duration(10.0);
 element31.duration(10.0);
 element32.duration(10.0);
 element33.duration(10.0);
 element34.duration(10.0);
+
+ const animation2 = new AnimationModel(AnimationType.PRESENTATION_ZOOMIN, 'Zoomin');
+animation2.start(0);
+animation2.duration(10);
+element31.add(animation2);
 const scene3 = new Scene("Comparison of car acceleration in three countries", 10.0);
+scene3.addElement(element333);
 scene3.addElement(element31);
 scene3.addElement(element32);
 scene3.addElement(element33);
