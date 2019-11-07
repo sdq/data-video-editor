3b3df2794fe423e9925bfe345ab551e2264d5a73
sdq <sdq@tongji.edu.cn>
2019-11-07 15:18:55 +0800

    chart update

--- src/charts/D3/D3Chart.js
+++ src/charts/D3/D3Chart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ChartImage from '../ChartImage';
+import { AnimationCreator } from '@/animation';
 import canvg from 'canvg';
 
 export default class D3Chart extends Component {
@@ -13,17 +14,19 @@ export default class D3Chart extends Component {
     }
 
     componentDidMount() {
-        const visSource = this.props.draw(this.props);
-        const chartImageUrl = this.getImageUrl(visSource);
-        this.setState({
-            specString: JSON.stringify(this.props.spec),
-            chartImageUrl: chartImageUrl
-        })
+        this.renderChart();
     }
 
     componentDidUpdate() {
         if (this.state.specString !== JSON.stringify(this.props.spec)) {
-            const visSource = this.props.draw(this.props);
+            this.renderChart();
+        }
+    }
+
+    renderChart = () => {
+        const svg = this.props.draw(this.props);
+        if (svg) {
+            const visSource = svg.node().parentNode.innerHTML;
             const chartImageUrl = this.getImageUrl(visSource);
             this.setState({
                 specString: JSON.stringify(this.props.spec),
@@ -36,14 +39,31 @@ export default class D3Chart extends Component {
         var canvas = document.createElement('canvas');
         canvas.width = this.props.width;
         canvas.height = this.props.height;
-        source = '<svg>' + source + '</svg>';
         canvg(canvas, source);
         return canvas.toDataURL('image/png');
     }
 
+    getImageRef = (ref) => {
+        if (!this.imageref && ref) {
+            this.imageref = ref;
+            const animations = this.props.animations; 
+            if (this.props.showAnimation && animations.length !== 0) {
+                let animationCreator = new AnimationCreator(ref);
+                for (let index = 0; index < animations.length; index++) {
+                    let current = this.props.current;
+                    if (this.props.isVideoPerforming) {
+                        current = 0;
+                    }
+                    const animation = animations[index];
+                    animationCreator.fromModel(animation).play(current);
+                }
+            }
+        } 
+    }
+
     render() {
         if (this.props.onCanvas) {
-            return <ChartImage name={this.props.name} src={this.state.chartImageUrl} />;
+            return <ChartImage name={this.props.name} src={this.state.chartImageUrl} getImageRef={this.getImageRef}/>;
         } else {
             return <div className={this.props.chartId}/>;
         }
