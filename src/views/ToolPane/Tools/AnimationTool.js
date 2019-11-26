7cb56e23bf02a397a151f4b8c6378163c5ca2a1a
YueyaoZhang <zhangyueyao@whu.edu.cn>
2019-11-26 11:26:18 +0800

    完善居中策略

--- src/views/ToolPane/Tools/AnimationTool.js
+++ src/views/ToolPane/Tools/AnimationTool.js
@@ -3,18 +3,15 @@ import React, { Component } from 'react';
 import { AnimationList } from '@/animation';
 import AnimationCard from '@/components/AnimationCard';
 
-// const { Panel } = Collapse;
-
-const paneHeight = 290;
 
 export default class AnimationTool extends Component {
 
 
     render() {
         return (
-            <div style={{padding: '0px 10px 10px 10px', fontSize: '14px', backgroundColor: 'white',height:'400px',overflow: 'auto'}}>
-                <div style={{height:1, width: 300, backgroundColor: '#d8d8d8'}}/>
-                <div style={{height: paneHeight}}>
+            <div style={{padding: '0px 10px 10px 10px', fontSize: '14px', backgroundColor: 'white',height:this.props.contentHeight-140+'px',overflow: 'auto'}}>
+                <div style={{ width: 300, backgroundColor: '#d8d8d8'}}/>
+                <div>
                     {AnimationList.all.map((animation, index) =>
                         <AnimationCard key={index} animation={animation} {...this.props}/>
                     )}
